d127302022373d58921f21c6b50f835af29739c7
davidelong0 <davidelong0@hotmail.com>
2025-06-30 17:24:05 +0200

    inserito backend

---
+++ GratisEscapeBackend/src/main/java/com/example/gratisescape/GratisEscapeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gratisescape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GratisEscapeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GratisEscapeBackendApplication.class, args);
+	}
+
+}
---
+++ GratisEscapeBackend/src/test/java/GratisEscapeBackend/GratisEscapeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package GratisEscapeBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GratisEscapeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

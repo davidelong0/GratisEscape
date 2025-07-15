7bca814a2b26e95ee8b9ebecff56b94407757637
davidelong0 <davidelong0@hotmail.com>
2025-07-15 12:54:23 +0200

    navabr corretta per home utente e admin

--- gratisescapefrontend/src/components/layout/Navbar.jsx
+++ gratisescapefrontend/src/components/layout/Navbar.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../../redux/actions/authActions';
 import { toast } from 'react-toastify';
+import { ArrowLeft } from 'react-bootstrap-icons';
 
 const Navbar = () => {
   const { user } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const [showBackArrow, setShowBackArrow] = useState(false);
+  const isAdmin = user?.ruolo === 'ADMIN';
+
+  useEffect(() => {
+    setShowBackArrow(location.pathname !== '/');
+  }, [location]);
 
   const handleLogout = () => {
     dispatch(logout());
@@ -17,9 +26,18 @@ const Navbar = () => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary px-4">
+      {showBackArrow && (
+        <button
+          className="btn btn-outline-light me-2"
+          onClick={() => navigate(-1)}
+        >
+          <ArrowLeft />
+        </button>
+      )}
       <Link className="navbar-brand" to="/">
         GratisEscape
       </Link>
+
       <div className="collapse navbar-collapse">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
@@ -28,7 +46,7 @@ const Navbar = () => {
             </Link>
           </li>
 
-          {user && (
+          {user && !isAdmin && (
             <>
               <li className="nav-item">
                 <Link className="nav-link" to="/richiesta">
@@ -48,7 +66,7 @@ const Navbar = () => {
             </>
           )}
 
-          {user?.ruolo === 'ADMIN' && (
+          {isAdmin && (
             <>
               <li className="nav-item">
                 <Link className="nav-link" to="/admin/viaggi">
@@ -64,32 +82,38 @@ const Navbar = () => {
           )}
         </ul>
 
-        <ul className="navbar-nav ms-auto">
-          {!user ? (
-            <>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/register">
-                  Registrati
-                </Link>
-              </li>
-            </>
-          ) : (
+        {user && (
+          <ul className="navbar-nav ms-auto align-items-center">
+            <li className="nav-item text-white me-3">
+              {isAdmin ? "Admin" : `${user.nome} ${user.cognome}`}
+            </li>
             <li className="nav-item">
               <button className="btn btn-outline-light" onClick={handleLogout}>
                 Logout
               </button>
             </li>
-          )}
-        </ul>
+          </ul>
+        )}
+
+        {!user && (
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/register">
+                Registrati
+              </Link>
+            </li>
+          </ul>
+        )}
       </div>
     </nav>
   );
 };
 
 export default Navbar;
+
 
